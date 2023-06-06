800c583cadebab8c1a6ac64aad16e44d2255a625
SerhiiMonzhelesov <monzhelesov@gmail.com>
2023-06-07 00:34:43 +0300

    js hw 7 task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,20 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const markupGallery = galleryItems.map(({ preview, original, description }) => {
+    return `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`
+}).join('')
+
+const listGallery = document.querySelector('.gallery')
+listGallery.insertAdjacentHTML('afterbegin', markupGallery)
+
+listGallery.addEventListener('click', handlerPreview)
